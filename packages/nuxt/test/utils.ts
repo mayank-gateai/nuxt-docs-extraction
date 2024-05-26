f681583542a0c77cc97467c36ac7aa52966ebb90
Mayank Sharma <154227121+mayank-gateai@users.noreply.github.com>
2024-05-26 08:00:25 +0530

    segregatted official docs from the repo (#1)

--- packages/nuxt/test/utils.ts
+++
@@ -1,7 +0,0 @@
-import { resolve } from 'pathe'
-
-export const fixtureDir = resolve(__dirname, 'fixture')
-
-export function normalizeLineEndings (str: string, normalized = '\n') {
-  return str.replace(/\r?\n/g, normalized)
-}
